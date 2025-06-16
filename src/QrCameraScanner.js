ef0e280c7634afc9dd87ad8350a9e216f16e2897
sajidhgn <sajidhgn@gmail.com>
2025-06-16 12:27:59 +0500

    fix repo

--- src/QrCameraScanner.js
+++ src/QrCameraScanner.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react';
+import { useRef, useEffect, useState, useCallback } from 'react';
 import jsQR from 'jsqr';
 
 const QrCameraScanner = ({
